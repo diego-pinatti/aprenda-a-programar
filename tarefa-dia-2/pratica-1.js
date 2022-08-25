38a62b0dc491cd03c03cf023bee45a2b1d3d8b03
Diego Pinatti Batista <Diego.pinatti87@yahoo.com>
2022-08-25 18:52:33 -0300

    informações pessoais atualizadas

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,7 @@
 var nome = 'Diego'
 var email = 'diego.pinatti87@gmail.com'
 var profissao = 'analista de configuração'
-var idade = '35 anos'
+var idade = '35'
 var lugar = 'Japão'
+
+console.log(`Seu nome é ${nome}, seu email é ${email}. Ele trabalha como ${profissao} e tem ${idade} anos. Ele sonha em conhecer o ${lugar} algum dia.`)
