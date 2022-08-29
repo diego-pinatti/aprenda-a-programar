07e0e0bd1c835b7151540efc03504c50b84d2dbb
Diego Pinatti Batista <Diego.pinatti87@yahoo.com>
2022-08-29 17:45:54 -0300

    pratica 2 concluida

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 9
+let i = 1
+let n = 10
+
+while (i <= n) {
+    let resultado = multiplicador * i
+    let exibir = `${multiplicador} X ${i} = ${resultado}`
+    console.log(exibir)
+    i++
+}
